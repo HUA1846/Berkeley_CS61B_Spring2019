a015fe7df95be7ad7afde5f3073f93e3610633ef
HUA1846 <hua1378246@gmail.com>
2021-01-25 09:49:48 -0800

    debug changePriority, improve time efficiency

--- proj2ab/bearmaps/ArrayHeapMinPQ.java
+++ proj2ab/bearmaps/ArrayHeapMinPQ.java
@@ -1,12 +1,13 @@
 package bearmaps;
 
-import java.util.NoSuchElementException;
+import java.util.*;
 
 public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
     private int capacity;
     private Entry[] minHeap;
     private int size;
     private int pos;
+    private Map<T, Integer> keys = new HashMap<>();
 
     public ArrayHeapMinPQ(int capacity) {
         this.capacity = capacity;
@@ -23,6 +24,9 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
             item = i;
             priority = p;
         }
+        public void setPriority(double p) {
+            this.priority = p;
+        }
     }
 /* Adds an item of type T with the given priority. If the item already exists,
    throw an IllegalArgumentException. You may assume that item is never null. */
@@ -32,6 +36,7 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
             throw new IllegalArgumentException("this item already exists");
         }
         minHeap[pos] = new Entry(item, priority);
+        keys.put(item, pos);
         bubbleUp(pos);
         pos += 1;
         size += 1;
@@ -44,17 +49,12 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
     /* Returns true if the PQ contains the given item. */
     @Override
     public boolean contains(T item) {
-        for(int i = 1; i < pos; i++) {
-            if(minHeap[i].item.equals(item)) {
-                return true;
-            }
-        }
-        return false;
+        return keys.containsKey(item);
     }
     /* Returns the item with smallest priority. If no items exist, throw a NoSuchElementException. */
     @Override
     public T getSmallest() {
-        if(isEmpty()) {
+        if(size == 0) {
             throw new NoSuchElementException();
         }
         return (T) minHeap[1].item;
@@ -62,10 +62,11 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
     /* Removes and returns the item with smallest priority. If no items exist, throw a NoSuchElementException */
     @Override
     public T removeSmallest() {
-        if(isEmpty()) {
+        if(size == 0) {
             throw new NoSuchElementException();
         }
         T returnItem = (T)minHeap[1].item;
+        keys.remove(returnItem);
         minHeap[1] = minHeap[pos - 1];
         minHeap[pos - 1] = null;
         pos -= 1;
@@ -81,26 +82,26 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
         return size;
     }
 
-    private boolean isEmpty() {
-        return size == 0;
-    }
+
     /* Changes the priority of the given item. Throws NoSuchElementException if the item
      * doesn't exist. */
     @Override
     public void changePriority(T item, double priority) {
-        for(int i = 1; i < pos; i++) {
-            if(minHeap[i].item.equals(item)) {
-                minHeap[i].priority = priority;
-                if(priority < minHeap[parentIdx(i)].priority) {
-                    bubbleUp(i); return;
-                }
-                if(priority > minHeap[i*2].priority || priority > minHeap[i*2 + 1].priority) {
-                    sinkDown(i); return;
-                }
-            }
+        if(!contains(item)) {
+            throw new NoSuchElementException();
         }
-        throw new NoSuchElementException();
+        int index = find(item);
+        minHeap[index].setPriority(priority);
+        if(minHeap[index].priority < minHeap[parentIdx(index)].priority) {
+            bubbleUp(index);
+        } else {
+            sinkDown(index);
+        }
     }
+    private int find(T item) {
+        return keys.get(item);
+    }
+
     private void resize() {
         if(size == minHeap.length - 1) {
             capacity *= 2;
@@ -140,10 +141,15 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
     private void swap(Entry e1, Entry e2) {
         T temp = (T)e1.item;
         double p = e1.priority;
+        int index1 = find((T)e1.item);
+        int index2 = find((T)e2.item);
+        keys.replace((T) e1.item, index2);
+        keys.replace((T) e2.item, index1);
         e1.item = e2.item;
         e1.priority = e2.priority;
         e2.item = temp;
         e2.priority = p;
+
     }
 
     private int parentIdx(int n) {
--- proj2ab/bearmaps/ArrayHeapMinPQTest.java
+++ proj2ab/bearmaps/ArrayHeapMinPQTest.java
@@ -2,6 +2,7 @@ package bearmaps;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import edu.princeton.cs.algs4.Stopwatch;
 
 public class ArrayHeapMinPQTest {
 
@@ -21,7 +22,7 @@ public class ArrayHeapMinPQTest {
     }
 
     @Test
-    public void testRemoveSmalles() {
+    public void testRemoveSmallest() {
         ArrayHeapMinPQ<String> pq = new ArrayHeapMinPQ<>(16);
         pq.add("Sunday", 7);
         pq.add("Saturday", 6);
@@ -52,6 +53,22 @@ public class ArrayHeapMinPQTest {
         pq.add("Monday", 1);
         pq.changePriority("Monday", 8);
         assertEquals("Tuesday", pq.getSmallest());
+        pq.changePriority("Sunday", 1);
+        assertEquals("Sunday", pq.getSmallest());
+
+        ArrayHeapMinPQ<String> pq2 = new ArrayHeapMinPQ<>(16);
+        for(int i = 0; i < 100; i++) {
+            pq2.add("item" + i, i);
+        }
+        for(int i = 0; i < 100; i ++) {
+            pq2.changePriority("item" + i, 99-i);
+        }
+        assertEquals("item99", pq2.getSmallest());
+        assertTrue(pq2.contains("item99"));
+        assertEquals("item99", pq2.removeSmallest());
+        assertEquals("item98", pq2.getSmallest());
+        assertEquals(99, pq2.size());
+        assertFalse(pq2.contains("item99"));
     }
 
     @Test
@@ -68,5 +85,30 @@ public class ArrayHeapMinPQTest {
         }
         assertEquals(50, pq.size());
         assertEquals("item50", pq.getSmallest());
+    }
+
+    public void naivePQTime() {
+        NaiveMinPQ<String> naive = new NaiveMinPQ<String>();
+        for(int i = 0; i < 200; i++) {
+            naive.add("item" + i, i);
+        }
+
+    }
+    public void arrayPQTime() {
+        Stopwatch sw = new Stopwatch();
+        ArrayHeapMinPQ<String> pq = new ArrayHeapMinPQ<>(16);
+        for(int i = 0; i < 200; i++) {
+            pq.add("item" + i, i);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ArrayHeapMinPQTest pqTest = new ArrayHeapMinPQTest();
+        Stopwatch sw = new Stopwatch();
+        pqTest.naivePQTime();
+        System.out.println("NaiveMinPQ " + sw.elapsedTime());
+        pqTest.arrayPQTime();
+        System.out.println("ArrayHeapMinPQ " + sw.elapsedTime());
     }
 }
